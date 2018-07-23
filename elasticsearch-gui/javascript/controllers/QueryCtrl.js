4a49ae06323b3f77150d07dbd7003916d7d65222
Ari Hershowitz <arihershowitz@Aris-MacBook-Pro.local>
2018-07-23 02:20:03 -0500

    Add Property title option

--- elasticsearch-gui/javascript/controllers/QueryCtrl.js
+++ elasticsearch-gui/javascript/controllers/QueryCtrl.js
@@ -21,7 +21,7 @@ function QueryCtrl($scope, $modal, elastic, aggregateBuilder, queryStorage) {
 
     $scope.query.doctypes = [{'name':'bill'},{'name':'act'},{'name':'amendment'}];
 
-    $scope.query.subjects={0:{'name':'Courts','forPrint':'Courts, Civil Procedure(1-55)'},1:{'name':'Business Organizations, Commercial Code','forPrint':'Business Organizations, Commercial Code(56-88)'},2:{'name':'Landlord-Tenant, Domestic Relations, Probate','forPrint':'Landlord-Tenant, Domestic Relations, Probate (90-131A)'}, 3:{'name':'...','forPrint':'...'}};
+    $scope.query.subjects={0:{'name':'Courts','forPrint':'Courts, Civil Procedure(1-55)'},1:{'name':'Business Organizations, Commercial Code','forPrint':'Business Organizations, Commercial Code(56-88)'},2:{'name':'Landlord-Tenant, Domestic Relations, Probate','forPrint':'Landlord-Tenant, Domestic Relations, Probate (90-131A)'}, 3:{'name':'...','forPrint':'...'}, 4:{'name':'Eminent Domain; Public Acquisition of Property','forPrint':'Eminent Domain; Public Acquisition of Property'}};
 
 
     // initialize pagination
